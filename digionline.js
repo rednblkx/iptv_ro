11e744cdc041721f543b0838f18d77a306f3e354
White Red <rednblkx@protonmail.com>
2021-02-03 04:42:24 +0200

    fixed login on digi

--- digionline.js
+++ digionline.js
@@ -201,6 +201,8 @@ async function login() {
             'Origin': 'https://www.digionline.ro',
             'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36"
           },
+          maxRedirects: 0,
+          validateStatus: (status) => status === 302
         })
         auth.digi.cookies = [];
         log.headers['set-cookie'].forEach(cookie => {
